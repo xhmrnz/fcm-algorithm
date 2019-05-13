109641f2347e412fa7d1be624d7c2818196d5c93
unknown <system@ilhamriana.com>
2019-05-14 02:25:11 +0700

    update

--- databs.php
+++ databs.php
@@ -133,7 +133,7 @@
 
           <!-- Page Heading -->
           <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-          <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
+          <p class="mb-4">Harap masukan data terlebih dahulu sebelum memulai proses perhitungan.</a>.</p>
           
 
           <!-- DataTales Example -->
